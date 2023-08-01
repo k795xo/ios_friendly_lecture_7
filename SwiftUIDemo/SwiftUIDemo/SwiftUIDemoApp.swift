dd01f9bb3eb13034e4319209b0eba1cf1649a685
K Chizhov <k795xo@gmail.com>
2023-08-01 13:53:47 +0300

    Создание проекта

---
+++ SwiftUIDemo/SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Konstantin Chizhov on 01.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
